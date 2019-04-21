8567e51c9a8865caff611a8e539e3e5994ede9e4
usernameHed <ugo@belfiore.fr>
2019-04-21 05:06:16 +0200

    clean up

--- Assets/_Scripts/Extentions/ExtTexture.cs
+++ Assets/_Scripts/Extentions/ExtTexture.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class ExtTexture : MonoBehaviour
+public static class ExtTexture
 {
     /// <summary>
     /// Saves a texture to PNG.
